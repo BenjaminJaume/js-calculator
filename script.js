72ca82262ee092c24a784dfbad64770a7c9e63dd
BenjaminJaume <benjamin.jaume@gmail.com>
2019-10-16 22:21:45 +1100

    Adding JS lines

--- script.js
+++ script.js
@@ -1,32 +1,88 @@
 let result = 0;
-let temp = 0;
+let buffer = '';
+let symbolClicked = '';
+let resultDisplayed = document.querySelector('.result');
 
 document
   .querySelector('.calculator')
   .addEventListener('click', function(event) {
     if (event.target.tagName === 'BUTTON') {
-      switch (event.target.innerHTML) {
-        case 'C':
-          clear();
-          break;
-
-        case '←':
-          console.log('You clicked ←');
-          break;
-
-        default:
-          console.log('Error');
-          break;
-      }
+      buttonClick(event.target.innerHTML);
     }
   });
 
-var updateDisplay = props => {
-  let resultDisplayed = document.querySelector('.result');
-  resultDisplayed.innerHTML = props;
+// Function called each time a button is clicked
+// It's making the difference between a symbol and
+// a number
+var buttonClick = data => {
+  if (isNaN(data)) {
+    handleSymbol(data);
+  } else {
+    handleNumber(data);
+  }
 };
 
+// If the button clicked is NaN (so it's a Symbol)
+var handleSymbol = data => {
+  if (data === 'C') {
+    clear();
+  } else if (data === '←') {
+    deleteOne();
+  } else if (data === '=') {
+    equals();
+  } else {
+    // Else it's a math symbol
+    symbolClicked = data;
+    buffer = resultDisplayed.innerHTML;
+    resultDisplayed.innerHTML = '0';
+  }
+};
+
+var handleNumber = data => {
+  if (resultDisplayed.innerHTML === '0') {
+    result = data;
+  } else {
+    result += data;
+  }
+  updateDisplay(result);
+};
+
+var equals = () => {
+  if (symbolClicked === '+') {
+    result = +parseInt(buffer) + parseInt(resultDisplayed.innerHTML);
+  } else if (symbolClicked === '-') {
+    result = parseInt(buffer) - parseInt(resultDisplayed.innerHTML);
+  } else if (symbolClicked === '/') {
+    result = parseInt(buffer) / parseInt(resultDisplayed.innerHTML);
+  } else if (symbolClicked === '*') {
+    result = parseInt(buffer) * parseInt(resultDisplayed.innerHTML);
+  }
+  updateDisplay(result);
+};
+
+var updateDisplay = props => {
+  resultDisplayed.innerHTML = props.toString();
+};
+
+// Clear the calculator
 var clear = () => {
   result = 0;
+  buffer = '';
+  symbolClicked = '';
+  updateDisplay(result);
+};
+
+// Delete one character at the end
+var deleteOne = () => {
+  if (parseInt(resultDisplayed.innerHTML) > 9) {
+    resultDisplayed.innerHTML = resultDisplayed.innerHTML.slice(
+      0,
+      resultDisplayed.innerHTML.length - 1
+    );
+    result = parseInt(resultDisplayed.innerHTML);
+  } else {
+    resultDisplayed.innerHTML = '0';
+    result = 0;
+  }
   updateDisplay(result);
 };
